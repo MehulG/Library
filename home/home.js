48e449836dfab3a3814f1af70993ddda12cec306
Unknown <mehulgaidhani@gmail.com>
2018-03-09 09:37:18 -0800

    alphabetical search

--- home/home.js
+++ home/home.js
@@ -8,12 +8,25 @@ $(document).ready(function(){
     $('#s_subject').val('');
 
     $.ajax({url: "", success: function(result){
-        $("#main_table").append(`<tr><td>`+element.ISSN+`</td><td><a href="">`+ element.Title +`</a> </td><td><a href="">`+element.Publisher+`</a> </td><td><a href="">`+element.Accessible_from+`</a> </td><td><a href="">`+element.Accessible_upto+`</a></td><td><a href="`+element.URL+`">`+element.URL+`</a></td><td><a href="">`+element.Subject+`</a></td></tr>`);
+        result.forEach(element => {
+            $("#main_table").append(`<tr><td>`+element.ISSN+`</td><td><a href="">`+ element.Title +`</a> </td><td><a href="">`+element.Publisher+`</a> </td><td><a href="">`+element.Accessible_from+`</a> </td><td><a href="">`+element.Accessible_upto+`</a></td><td><a href="`+element.URL+`">`+element.URL+`</a></td><td><a href="">`+element.Subject+`</a></td></tr>`);
+        });
+        
+        for(var i=0; i<=result[0].no_of_pages; i++){
+            if (i!=result[0].page_no) {
+                $('#pages').append(`<button >`+i+`</button>`);                        
+            }else{
+                $('#pages').append(i);                        
+            }
+        }
+
     }});
 
 
     //functions
 
+
+//search
     $("#sub1").click(function(){
 
         var title = $('#s_title').val();
@@ -24,8 +37,52 @@ $(document).ready(function(){
                 $("#main_table").append(`<tr><td>`+element.ISSN+`</td><td><a href="">`+ element.Title +`</a> </td><td><a href="">`+element.Publisher+`</a> </td><td><a href="">`+element.Accessible_from+`</a> </td><td><a href="">`+element.Accessible_upto+`</a></td><td><a href="`+element.URL+`">`+element.URL+`</a></td><td><a href="">`+element.Subject+`</a></td></tr>`);
             });
             
-            for(var i=0; i<=element.no_of_pages; i++){
-                if (i!=element.page_no) {
+            for(var i=0; i<=result[0].no_of_pages; i++){
+                if (i!=result[0].page_no) {
+                    $('#pages').append(`<button >`+i+`</button>`);                        
+                }else{
+                    $('#pages').append(i);                        
+                }
+            }
+
+        }});
+    });
+
+//clear
+    $("#sub2").click(function(){
+
+        var title = '';
+        var subj ='';
+        $('#s_title').val('');
+        $('#s_subject').val('');
+
+        $.ajax({url: "", success: function(result){
+            result.forEach(element => {
+                $("#main_table").append(`<tr><td>`+element.ISSN+`</td><td><a href="">`+ element.Title +`</a> </td><td><a href="">`+element.Publisher+`</a> </td><td><a href="">`+element.Accessible_from+`</a> </td><td><a href="">`+element.Accessible_upto+`</a></td><td><a href="`+element.URL+`">`+element.URL+`</a></td><td><a href="">`+element.Subject+`</a></td></tr>`);
+            });
+            
+            for(var i=0; i<=result[0].no_of_pages; i++){
+                if (i!=result[0].page_no) {
+                    $('#pages').append(`<button >`+i+`</button>`);                        
+                }else{
+                    $('#pages').append(i);                        
+                }
+            }
+
+        }});
+    });
+
+//different page
+    $('#pages').children('button').click(function(){
+        var Page_no = $(this).html();
+
+        $.ajax({url: "", success: function(result){
+            result.forEach(element => {
+                $("#main_table").append(`<tr><td>`+element.ISSN+`</td><td><a href="">`+ element.Title +`</a> </td><td><a href="">`+element.Publisher+`</a> </td><td><a href="">`+element.Accessible_from+`</a> </td><td><a href="">`+element.Accessible_upto+`</a></td><td><a href="`+element.URL+`">`+element.URL+`</a></td><td><a href="">`+element.Subject+`</a></td></tr>`);
+            });
+            
+            for(var i=0; i<=result[0].no_of_pages; i++){
+                if (i!=result[0].page_no) {
                     $('#pages').append(`<button >`+i+`</button>`);                        
                 }else{
                     $('#pages').append(i);                        
@@ -36,27 +93,30 @@ $(document).ready(function(){
     });
 
 
-    $("#sub2").click(function(){
 
-        var title = '';
-        var subj ='';
-        $('#s_title').val('');
-        $('#s_subject').val('');
-
-        $.ajax({url: "", success: function(result){
+//alphabetical
+$('#top').children('button').click(function(){
+    var alphabet = $(this).html();
+    console.log(alphabet);
+    
+    $.ajax({url: "", success: function(result){
+        result.forEach(element => {
             $("#main_table").append(`<tr><td>`+element.ISSN+`</td><td><a href="">`+ element.Title +`</a> </td><td><a href="">`+element.Publisher+`</a> </td><td><a href="">`+element.Accessible_from+`</a> </td><td><a href="">`+element.Accessible_upto+`</a></td><td><a href="`+element.URL+`">`+element.URL+`</a></td><td><a href="">`+element.Subject+`</a></td></tr>`);
-        }});
-    });
-
-
-    $('#pages').children('button').click(function(){
-        var Page_no = $(this).html();
-
-        $.ajax({url: "", success: function(result){
-            $("#main_table").append(`<tr><td>`+element.ISSN+`</td><td><a href="">`+ element.Title +`</a> </td><td><a href="">`+element.Publisher+`</a> </td><td><a href="">`+element.Accessible_from+`</a> </td><td><a href="">`+element.Accessible_upto+`</a></td><td><a href="`+element.URL+`">`+element.URL+`</a></td><td><a href="">`+element.Subject+`</a></td></tr>`);
-        }});
+        });
         
-    });
+        for(var i=0; i<=result[0].no_of_pages; i++){
+            if (i!=result[0].page_no) {
+                $('#pages').append(`<button >`+i+`</button>`);                        
+            }else{
+                $('#pages').append(i);                        
+            }
+        }
+
+    }});
+});
+
+
+
 
 
 });
